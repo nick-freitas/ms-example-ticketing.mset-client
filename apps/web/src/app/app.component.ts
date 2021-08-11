4d7e680b222301167de1ff017fb3ddfaf3f9607a
Nick Freitas <nickfrei@gmail.com>
2021-08-10 23:43:04 -0400

    init commit

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mset-client-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
